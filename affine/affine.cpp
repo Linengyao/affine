83f4e2f63c50568479ee1b8902991261a0753fb6
Linengyao <1547784788@qq.com>
2020-04-03 15:04:30 +0800

    4

--- affine/affine.cpp
+++ affine/affine.cpp
@@ -2,10 +2,37 @@
 //
 
 #include <iostream>
+#include <opencv.hpp>
 
+using namespace cv;
+using namespace std;
 int main()
 {
-    std::cout << "Hello World!\n";
+	cv::Mat dstMat;
+	cv::Mat	srcMat = cv::imread("E:\\课程\\大二下\\数字图像处理\\lena.jpg", 1);
+	if (srcMat.empty())	return -1;
+
+
+
+	//变换前的四点坐标
+	cv::Point2f src_pt[] = {
+						cv::Point2f(200,200),
+						cv::Point2f(250,200),
+						cv::Point2f(200,100), };
+
+	//变换后的四点坐标
+	cv::Point2f dst_pt[] = {
+						cv::Point2f(300,100),
+						cv::Point2f(300,50),
+						cv::Point2f(200,100), };
+	const cv::Mat affine_marix = cv::getAffineTransform(src_pt, dst_pt);
+	cv::warpAffine(srcMat, dstMat, affine_marix, srcMat.size());
+
+	cv::imshow("src", srcMat);
+	cv::imshow("dst", dstMat);
+	cv::waitKey(0);
+
+	//std::cout << "Hello World!\n";
 }
 
 // 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
